4c26ccc26f8c704566c5911ad1c9e805b3c27503
HunterVH <hunter.vanhorn76@gmail.com>
2025-04-18 00:26:49 -0600

    The audio function now uses the h(t) function for the convolution instead of the original H(s) function.

--- Lab_9/lab9.py
+++ Lab_9/lab9.py
@@ -19,13 +19,15 @@ def plotBode(denominator, numerator):
 
     plt.show()
 
-def audio(lowPass, highPass):
-    sysLP = signal.TransferFunction(lowPass[0], lowPass[1])
-    w, h_lp, phase = signal.bode(sysLP)
-    sysHP = signal.TransferFunction(highPass[0], highPass[1])
-    w, h_hp, phase = signal.bode(sysHP)
+def audio():
+    def subwoofer(t):
+        return 1-2500*np.exp(-1250*t)*np.cos(np.sqrt(198437500)*t)-(196875000/np.sqrt(198437500))*np.exp(-1250*t)*np.sin(np.sqrt(198437500)*t)
+    def tweeter(t):
+        return (200000000/np.sqrt(43750000))*np.exp(-12500*t)*np.sin(np.sqrt(43750000)*t)
 
-    print(type(sysLP))
+    time = np.arange(0, .004, .00001)
+    h_lp = subwoofer(time)
+    h_hp = tweeter(time)
 
     filename = 'handel.wav'
     data, fs = sf.read(filename, dtype='float32')
@@ -50,14 +52,14 @@ def main():
     R = 7.15
     denomLP = [1/(L*C)]
     numerLP = [1, 1/(R*C), 1/(L*C)]
-    # plotBode(denomLP, numerLP)
+    plotBode(denomLP, numerLP)
 
     L = 1E-3
     C = 5E-6
     R = 8
     denomHP = [1, 0, 0]
     numerHP = [1, 1/(R*C), 1/(L*C)]
-    # plotBode(denomHP, numerHP)
+    plotBode(denomHP, numerHP)
 
     audio((denomLP, numerLP), (denomHP, numerHP))
 
