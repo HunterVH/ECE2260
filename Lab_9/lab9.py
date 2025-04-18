54058e282bbde1ddb4f1ff4f90a9a40ecaced100
HunterVH <hunter.vanhorn76@gmail.com>
2025-04-17 22:01:24 -0600

    Added the audio function that is being used to play an audio file normally as well as through a high and low pass filter.

--- Lab_9/lab9.py
+++ Lab_9/lab9.py
@@ -1,6 +1,8 @@
 from scipy import signal
 import matplotlib.pyplot as plt
 import numpy as np
+import sounddevice as sd
+import soundfile as sf
 
 def plotBode(denominator, numerator):
     sys = signal.TransferFunction(denominator, numerator)
@@ -17,23 +19,47 @@ def plotBode(denominator, numerator):
 
     plt.show()
 
+def audio(lowPass, highPass):
+    sysLP = signal.TransferFunction(lowPass[0], lowPass[1])
+    w, h_lp, phase = signal.bode(sysLP)
+    sysHP = signal.TransferFunction(highPass[0], highPass[1])
+    w, h_hp, phase = signal.bode(sysHP)
+
+    print(type(sysLP))
+
+    filename = 'handel.wav'
+    data, fs = sf.read(filename, dtype='float32')
+    data = data[:,0]
+    data_lp = np.convolve(data, h_lp)
+    data_lp /= max(abs(data_lp))
+    data_hp = np.convolve(data, h_hp)
+    data_hp /= max(abs(data_hp))
+
+    sound_break = np.zeros(fs*2)
+
+    data_play = np.hstack((sound_break, data,
+                           sound_break, data_lp,
+                           sound_break, data,
+                           sound_break, data_hp))
+    sd.play(data_play, fs)
+    status = sd.wait()
+
 def main():
     L = .994E-3
     C = 52.3E-6
     R = 7.15
-
-    denom = [1/(L*C)]
-    numer = [1, 1/(R*C), 1/(L*C)]
-
-    plotBode(denom, numer)
+    denomLP = [1/(L*C)]
+    numerLP = [1, 1/(R*C), 1/(L*C)]
+    # plotBode(denomLP, numerLP)
 
     L = 1E-3
     C = 5E-6
     R = 8
+    denomHP = [1, 0, 0]
+    numerHP = [1, 1/(R*C), 1/(L*C)]
+    # plotBode(denomHP, numerHP)
 
-    denom = [1, 0, 0]
-    numer = [1, 1/(R*C), 1/(L*C)]
-    plotBode(denom, numer)
+    audio((denomLP, numerLP), (denomHP, numerHP))
 
 if __name__ == "__main__":
     main()
